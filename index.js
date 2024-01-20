7012d1dbd54f10f5324cab5c41a7122d29a8d122
Nitesh Kumar <108714909+Niteshkumar13@users.noreply.github.com>
2024-01-20 22:29:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const cors = require("cors");
 const socketIO = require("socket.io");
 const mongoose = require('mongoose');
 const { MongoClient } = require('mongodb');
-const url = '';
+const url = 'mongodb+srv://nitesh620877:nitesh620877@cluster1.hphom4r.mongodb.net/?retryWrites=true&w=majority';
 const dbName = 'database1';
 const collectionName = 'chat_data';
+const auth_collection = 'authentication'
 const client = new MongoClient(url);
 const app=express();
 app.use(express.json());
@@ -24,19 +25,46 @@ const server=http.createServer(app);
 const io=socketIO(server,{
     maxHttpBufferSize: 1e8 
   });
-  app.get('/data', async (req, res) => {
+  app.post('/data', async (req, res) => {
+    const {name,mobile_no,email} = req.body
     try {
         await client.connect();
         const db = client.db(dbName);
         const collection = db.collection(collectionName);
         const rows = await collection.find().toArray();
-        await client.close();
-        res.json(rows);
+        const collection_second = db.collection(auth_collection);
+        const rows_second = await collection_second.findOne({name:name,email:email,mobile_no:mobile_no});
+        // await client.close();
+        // console.log(rows_second);
+        if(rows_second){
+          res.json(rows);
+        }
+        else{
+          res.json({data:"you have not authority"})
+        }
+        
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: 'An error occurred' });
     }
 });
+app.post('/get-user-data', async (req, res) => {
+    
+    try {
+      await client.connect();
+      const db = client.db(dbName);
+      const collection = db.collection(auth_collection);
+      const rows = await collection.findOne({name:req.body.name,email: req.body.email,mobile_no:req.body.mobile_no});
+      console.log(rows)
+      if(rows){
+        res.json({...rows,...{name:req.body.name,email: req.body.email,mobile_no:req.body.mobile_no,next:true}});
+      }
+      
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'An error occurred' });
+    }
+  });
 
 io.on("connection", (socket)=>{
     console.log("New Connection");
@@ -49,14 +77,17 @@ io.on("connection", (socket)=>{
     socket.on('stream', (stream) => {
         socket.broadcast.emit('stream', stream);
       });
-     socket.on('msg',async({data,id,time, image,link,mobile_no})=>{
-        console.log({data,id,time, image,link,mobile_no});
-        io.emit('sendMessage',{user:users[id],data,id,time,image,link,mobile_no});
+     socket.on('msg',async({data,id,time, image,link,mobile_no,video,name,document,audio,last_date})=>{
+      if(mobile_no&&name){
+        console.log({data,id,time, image,link,mobile_no,video,name,document,audio,last_date});
+        io.emit('sendMessage',{user:users[id],data,id,time,image,link,mobile_no,video,name,document,audio,last_date});
         await client.connect();
         const db = client.db(dbName);
         const collection = db.collection(collectionName);
-        const result = await collection.insertOne({data,id,time, image,link,mobile_no});
+        const result = await collection.insertOne({data,id,time, image,link,mobile_no,video,name,document,audio,last_date});
         
+      }
+      
     })
 
     socket.on('disconnect',()=>{
